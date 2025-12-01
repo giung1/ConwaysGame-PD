dd8e0e11ea0360230d3ac943319d04864ca721f4
giung1 <agus_98_jov@hotmail.com>
2025-12-01 23:23:08 +0000

    fixed command line for installing tkinter

--- powerdevs-docker/assets/gen_pdm.sh
+++ powerdevs-docker/assets/gen_pdm.sh
@@ -8,7 +8,8 @@ python3 -c "import tkinter" 2>/dev/null
 # $? guarda el resultado del comando anterior (0 = éxito, 1 = error)
 if [ $? -ne 0 ]; then    
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sudo apt-get update && sudo apt-get install -y python3-tk
+        sudo apt-get update
+        sudo apt-get install -y python3-tk
     else
         echo "No se puede instalar tkinter automáticamente. Instala 'python3-tk' manualmente."
         exit 1
